066a0bd5786b9c6b3b4d80434edc7a9a8cf6a272
ithx <1445537716@qq.com>
2022-08-29 02:57:36 +0800

    登入界面

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,14 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
+// 登入接口封装
 export function login(data) {
+  // 返回一个axios对象 => promise  // 返回了一个promise对象
+  return request({
+    url: '/sys/login', // 因为所有的接口都要跨域 表示所有的接口要带 /api
+    method: 'post',
+    // data: data
+    data
+  })
 }
 
 export function getInfo(token) {
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,34 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+// 状态
+const state = {
+  token: getToken() // 设置token为共享状态,初始化vuex就先从缓存中读取
+}
+// 修改状态
+const mutations = {
+  // 设置Token
+  setToken(state, token) {
+    state.token = token // 将数据设置给vuex
+    // 同步给缓存
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null // 将vuex置空
+    removeToken() // 将缓存置空
+  }
+}
+// 执行异步
+const actions = {
+  async login(context, data) {
+    // 调用api接口
+    const result = await login(data) // 拿到token
+    // 如果为true 表示登入成功
+    context.commit('setToken', result) // 设置token
+  }
+}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
